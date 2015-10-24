bc8a594dfa781dbc4740f3e51862914cc0f42853
Sriram Kumar <ksriramleo@gmail.com>
2015-10-24 17:07:38 -0400

    first commit

---
+++ IOTDataService/src/main/java/com/hackathon/iot/controller/IOTDataServiceController.java
@@ -0,0 +1,11 @@
+package com.hackathon.iot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by srirkumar on 10/24/2015.
+ */
+@RestController
+public class IOTDataServiceController {
+
+}
