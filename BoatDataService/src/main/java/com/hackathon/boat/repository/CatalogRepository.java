3ca44105e6e1f32c62111aaf79617c2a9f3c4166
unknown <awmishra@L-BWI-00567377.paypalcorp.com>
2015-10-24 16:39:59 -0700

    Updating repository

--- BoatDataService/src/main/java/com/hackathon/boat/repository/CatalogRepository.java
+++ BoatDataService/src/main/java/com/hackathon/boat/repository/CatalogRepository.java
@@ -1,5 +1,6 @@
 package com.hackathon.boat.repository;
 
+import com.hackathon.boat.data.CatalogEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
@@ -8,5 +9,5 @@ import javax.transaction.Transactional;
  * Created by awmishra on 10/24/2015.
  */
 @Transactional
-public interface CatalogRepository extends CrudRepository <CatalogRepository, Long> {
+public interface CatalogRepository extends CrudRepository <CatalogEntity, Long> {
 }
